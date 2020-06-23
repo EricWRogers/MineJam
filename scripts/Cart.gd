70b57481bf6ed8e7f547dc4eccda4871bb0f87d5
EricWRogers <yerick147083@gmail.com>
2020-06-22 19:47:45 -0500

    player movement

--- scripts/Cart.gd
+++ scripts/Cart.gd
@@ -1,16 +1,21 @@
 extends Area2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+export var speed = 400
+var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	position += (velocity * speed) * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
